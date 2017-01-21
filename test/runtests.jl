135f45cd69d3e06272ed9c5bc263fb4213a82ca5
Asbjørn Nilsen Riseth <riseth@maths.ox.ac.uk>
2017-01-21 14:38:15 +0000

    Fix v0.6 depwarns

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,50 +4,50 @@ using Base.Test
 for pivot in (Val{false}, Val{true})
     A = [1 0; 0 1]
     F = cholfact(Positive, A, pivot)
-    @test_approx_eq full(F) A
+    @test full(F) ≈ A
     F, d = ldltfact(Positive, A, pivot)
-    @test_approx_eq full(F) A
+    @test full(F) ≈ A
     @test d == [1,1]
 
     A = [1 0; 0 -1]
     F = cholfact(Positive, A, pivot)
-    @test_approx_eq full(F) eye(2)
+    @test full(F) ≈ eye(2)
     F, d = ldltfact(Positive, A, pivot)
-    @test_approx_eq full(F) eye(2)
+    @test full(F) ≈ eye(2)
     @test d == [1,-1]
 
     A = [-1 0.5; 0.5 4]
     target = pivot == Val{false} ? [1 -0.5; -0.5 4.5] : [1.125 0.5; 0.5 4]
     dtarget = pivot == Val{false} ? [-1,1] : [1,-1]
     F = cholfact(Positive, A, pivot)
-    @test_approx_eq full(F) target
+    @test full(F) ≈ target
     F = cholfact(Positive, 10*A, pivot)
-    @test_approx_eq full(F) 10*target
+    @test full(F) ≈ 10*target
     F, d = ldltfact(Positive, A, pivot)
-    @test_approx_eq full(F) target
+    @test full(F) ≈ target
     @test d == dtarget
 
     A = [0 1; 1 0]
     F = cholfact(Positive, A, pivot)
-    @test_approx_eq full(F) eye(2)
+    @test full(F) ≈ eye(2)
     F, d = ldltfact(Positive, A, pivot)
-    @test_approx_eq full(F) eye(2)
+    @test full(F) ≈ eye(2)
     @test d == [0,0]
 
     A = rand(201,200); A = A'*A
     F = cholfact(Positive, A, pivot)
-    @test_approx_eq full(F) A
+    @test full(F) ≈ A
     F, d = ldltfact(Positive, A, pivot)
-    @test_approx_eq full(F) A
+    @test full(F) ≈ A
     @test all(d .== 1)
 end
 
 A = [1 0; 0 -2]
 F = eigfact(Positive, A)
-@test_approx_eq full(F) abs(A)
+@test full(F) ≈ abs.(A)
 A = [1 0; 0 0]
 F = eigfact(Positive, A)
-@test_approx_eq full(F) eye(2)
+@test full(F) ≈ eye(2)
 
 # Test whether necessary matrix operations are supported for SubArrays
 n = PositiveFactorizations.default_blocksize(Float64)
