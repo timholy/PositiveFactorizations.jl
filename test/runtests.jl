2dd8999696d6c13007e18b01bfb8916a165554ee
Asbjørn Nilsen Riseth <riseth@maths.ox.ac.uk>
2017-01-22 10:29:13 +0000

    test then compat

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,50 +5,50 @@ using Compat
 for pivot in (Val{false}, Val{true})
     A = [1 0; 0 1]
     F = cholfact(Positive, A, pivot)
-    @compat @test full(F) ≈ A
+    @test @compat full(F) ≈ A
     F, d = ldltfact(Positive, A, pivot)
-    @compat @test full(F) ≈ A
+    @test @compat full(F) ≈ A
     @test d == [1,1]
 
     A = [1 0; 0 -1]
     F = cholfact(Positive, A, pivot)
-    @compat @test full(F) ≈ eye(2)
+    @test @compat full(F) ≈ eye(2)
     F, d = ldltfact(Positive, A, pivot)
-    @compat @test full(F) ≈ eye(2)
+    @test @compat full(F) ≈ eye(2)
     @test d == [1,-1]
 
     A = [-1 0.5; 0.5 4]
     target = pivot == Val{false} ? [1 -0.5; -0.5 4.5] : [1.125 0.5; 0.5 4]
     dtarget = pivot == Val{false} ? [-1,1] : [1,-1]
     F = cholfact(Positive, A, pivot)
-    @compat @test full(F) ≈ target
+    @test @compat full(F) ≈ target
     F = cholfact(Positive, 10*A, pivot)
-    @compat @test full(F) ≈ 10*target
+    @test @compat full(F) ≈ 10*target
     F, d = ldltfact(Positive, A, pivot)
-    @compat @test full(F) ≈ target
+    @test @compat full(F) ≈ target
     @test d == dtarget
 
     A = [0 1; 1 0]
     F = cholfact(Positive, A, pivot)
-    @compat @test full(F) ≈ eye(2)
+    @test @compat full(F) ≈ eye(2)
     F, d = ldltfact(Positive, A, pivot)
-    @compat @test full(F) ≈ eye(2)
+    @test @compat full(F) ≈ eye(2)
     @test d == [0,0]
 
     A = rand(201,200); A = A'*A
     F = cholfact(Positive, A, pivot)
-    @compat @test full(F) ≈ A
+    @test @compat full(F) ≈ A
     F, d = ldltfact(Positive, A, pivot)
-    @compat @test full(F) ≈ A
+    @test @compat full(F) ≈ A
     @test all(d .== 1)
 end
 
 A = [1 0; 0 -2]
 F = eigfact(Positive, A)
-@compat @test full(F) ≈ abs.(A)
+@test @compat full(F) ≈ abs.(A)
 A = [1 0; 0 0]
 F = eigfact(Positive, A)
-@compat @test full(F) ≈ eye(2)
+@test @compat full(F) ≈ eye(2)
 
 # Test whether necessary matrix operations are supported for SubArrays
 n = PositiveFactorizations.default_blocksize(Float64)
